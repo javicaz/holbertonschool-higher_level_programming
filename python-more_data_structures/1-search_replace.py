aeec3c3b0faf6153bc494f5fbe1d083d426be0be
Javi Icaza <javiicaza@gmail.com>
2023-01-25 21:40:11 -0600

    search and replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
