4c30accc86f5049dabceababf78a5ca86673d9c7
Javi Icaza <javiicaza@gmail.com>
2023-01-30 23:54:23 -0600

    typo skip

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
     """defines the square"""
     def __init__(self, size=0):
         """Initialize data"""
-        if not ininstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
