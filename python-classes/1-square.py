e7e5f0844b96c3dba138c3a9657f01ad61dc683c
Javi Icaza <javiicaza@gmail.com>
2023-01-30 22:47:39 -0600

    create new size square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square."""
+
+
+class Square:
+    """Print the square."""
+
+    def _init_(self, size):
+        """Initialize a new data square."""
+        self._size = size
