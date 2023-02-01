7cdd9c5d458e2359b83ecfeb4f559ba05b838b21
Javi Icaza <javiicaza@gmail.com>
2023-01-31 23:12:44 -0600

    changing to args

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,46 +3,36 @@
 
 
 class Square:
-    """Defines a square
-    Private instance attribute: size:
-    property def size(self): to retrieve it
-    property setter def size(self, value): to set it:
-    size must be an integer, otherwise raise a TypeError exception
-    with the message size must be an integer
-    if size is less than 0, raise a ValueError exception with the
-    message size must be >= 0
-    Instantiation with optional size: def __init__(self, size=0):
-    Public instance method: def area(self): that returns the current
-    square area
-    """
-    def __init__(self, size=0):
-        """Initializes the data"""
-        self.__size = size
+    """Represent a square."""
 
-        @property
-        def size(self):
-            """Pulls the size"""
-            return self.__size
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
-        @size.setter
-        def size(self, value):
-            """Sets the size to a value"""
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        def area(self):
-            """Returns the current square area"""
-            return self.__size ** 2
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def my_print(self):
-            """Prints in stdout the square with the character #:"""
-            if self.__size == 0:
-                print("")
-            else:
-                for i in range(0, self.__size):
-                    for j in range(0, self.__size):
-                        print("#", end="")
-                print()
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
