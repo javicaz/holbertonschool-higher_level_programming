86505da53880ac38256aac465d91db0dca6af28e
Javi Icaza <javiicaza@gmail.com>
2023-01-31 22:59:48 -0600

    documented module

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,7 +3,18 @@
 
 
 class Square:
-    """Defines a square"""
+    """Defines a square
+    Private instance attribute: size:
+    property def size(self): to retrieve it
+    property setter def size(self, value): to set it:
+    size must be an integer, otherwise raise a TypeError exception
+    with the message size must be an integer
+    if size is less than 0, raise a ValueError exception with the
+    message size must be >= 0
+    Instantiation with optional size: def __init__(self, size=0):
+    Public instance method: def area(self): that returns the current
+    square area
+    """
     def __init__(self, size=0):
         """Initializes the data"""
         self.__size = size
@@ -27,7 +38,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Prints in stdout  the square with the character #:"""
+        """Prints in stdout the square with the character #:"""
         if self.__size == 0:
             print("")
         else:
